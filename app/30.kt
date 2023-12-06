92583cc434f5ea88652fbd898a78f1b718cc0473
BuildTools <unconfigured@null.spigotmc.org>
2023-12-07 00:22:13 +0200

    Initial commit

---
+++ app/30.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val m = 1000
+    val v = 10.0
+
+    val mInKg = m / 1000.0
+
+    val kineticEnergy = 0.5 * mInKg * v * v
+
+    println("Кинетическая энергия движущегося тела: $kineticEnergy Дж")
+}
